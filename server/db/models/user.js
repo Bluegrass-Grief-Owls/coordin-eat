c613b83e9b14bfd29a68fc73cb0a57400ba82524
Jackson Sui <kiro705@gmail.com>
2017-09-28 11:53:39 -0400

    added a seed file for users, friends, and locations

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -3,20 +3,24 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const User = db.define('user', {
-  email: {
-    type: Sequelize.STRING,
-    unique: true,
-    allowNull: false
-  },
-  password: {
-    type: Sequelize.STRING
-  },
-  salt: {
-    type: Sequelize.STRING
-  },
-  googleId: {
-    type: Sequelize.STRING
-  }
+	name: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	email: {
+		type: Sequelize.STRING,
+		unique: true,
+		allowNull: false
+	},
+	password: {
+		type: Sequelize.STRING
+	},
+	salt: {
+		type: Sequelize.STRING
+	},
+	googleId: {
+		type: Sequelize.STRING
+	}
 })
 
 module.exports = User
@@ -25,28 +29,28 @@ module.exports = User
  * instanceMethods
  */
 User.prototype.correctPassword = function (candidatePwd) {
-  return User.encryptPassword(candidatePwd, this.salt) === this.password
+	return User.encryptPassword(candidatePwd, this.salt) === this.password
 }
 
 /**
  * classMethods
  */
 User.generateSalt = function () {
-  return crypto.randomBytes(16).toString('base64')
+	return crypto.randomBytes(16).toString('base64')
 }
 
 User.encryptPassword = function (plainText, salt) {
-  return crypto.createHash('sha1').update(plainText).update(salt).digest('hex')
+	return crypto.createHash('sha1').update(plainText).update(salt).digest('hex')
 }
 
 /**
  * hooks
  */
 const setSaltAndPassword = user => {
-  if (user.changed('password')) {
-    user.salt = User.generateSalt()
-    user.password = User.encryptPassword(user.password, user.salt)
-  }
+	if (user.changed('password')) {
+		user.salt = User.generateSalt()
+		user.password = User.encryptPassword(user.password, user.salt)
+	}
 }
 
 User.beforeCreate(setSaltAndPassword)
