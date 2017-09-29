f9c6248ba566771fb02fb32ce84aafa96d05df6e
Sam <samuelglass@gmail.com>
2017-09-29 12:38:25 -0400

    results info in store

--- client/store/Results.js
+++ client/store/Results.js
@@ -18,6 +18,14 @@ const resultsArray = []
 
 const getResults = () => {type: GET_RESULTS, results}
 
+//THUNKS
+export const calculate = (array) =>
+	dispatch =>
+		axios.post('/api/midpoint', {places: array})
+			.then(res => dispatch(getResults(res)))
+			.catch(err => console.log(err))
+
+
 
 /**
  * REDUCER
@@ -25,7 +33,7 @@ const getResults = () => {type: GET_RESULTS, results}
 export default function (state = resultsArray, action) {
 	switch (action.type) {
 	case GET_RESULTS:
-		return action.results
+		return [...state, action.results]
 	default:
 		return state
 	}
