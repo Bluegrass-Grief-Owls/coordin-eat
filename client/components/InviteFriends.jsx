d3f0ad1fc5bae5417627b4deceadef34e1aab8d8
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-10-02 15:02:08 -0400

    Actually, InviteFriends can be a dumb comnponent

--- client/components/InviteFriends.jsx
+++ client/components/InviteFriends.jsx
@@ -5,26 +5,18 @@ import { Button, FormControl, FormGroup, ControlLabel } from 'react-bootstrap'
 /**
  * COMPONENT
  */
-class InviteFriends extends Component{
-	constructor(props){
-		super(props)
-		this.handleSubmit = this.props.handleSubmit.bind(this)
-	}
-
-	render(){
-		console.log("rendering")
-		return (
-			<div>
-				<form onSubmit={this.handleSubmit}>
-					<FormGroup controlId='email'>
-						<ControlLabel>Enter a friend's email</ControlLabel>
-						<FormControl type='text' />
-					</FormGroup>
-				</form>
-				<Button></Button>
-			</div>
-		)	
-	}
+const InviteFriends = (props) => {
+	return (
+		<div>
+			<form onSubmit={props.handleSubmit}>
+				<FormGroup controlId='email'>
+					<ControlLabel>Enter a friend's email</ControlLabel>
+					<FormControl type='text' />
+				</FormGroup>
+			</form>
+			<Button>Invite</Button>
+		</div>
+	)	
 }
 
 const mapDispatch = dispatch => {
