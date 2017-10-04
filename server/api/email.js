ce2269843ce59a4c69353d3917b1815e12b74ce0
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-10-04 11:14:27 -0400

    Email route is more secure

--- server/api/email.js
+++ server/api/email.js
@@ -1,5 +1,6 @@
 const nodeMailer = require('nodemailer')
 const router = require('express').Router()
+const {User} = require('../db/models')
 
 module.exports = router
 
@@ -13,12 +14,23 @@ let transporter = nodeMailer.createTransport({
 })
 
 router.post('/invite', (req, res) => { //just using post because it allows request to have a body
-	const url = 'http://localhost:8080/trips/' + req.body.tripName //or 'http://coordin-Eat.herokuapp.com/trips/ + req.body.tripName
+	if(req.body.tripName.match(/^[a-z0-9]+$/i)){ //check that the name is alphanumeric
+		const url = 'http://localhost:8080/trips/' + req.body.tripName //or 'http://coordin-Eat.herokuapp.com/trips/ + req.body.tripName
+		User.findById(req.body.user.id)
+			.then(user => {
+				sendMail(url, user.name, req.body.attendees, () => res.sendStatus(200))
+			})
+	} else {
+		res.sendStatus(403)
+	}
+})
+
+function sendMail(url, senderName, recipients, callback){
 	transporter.sendMail({
 		from: 'noreply@coordin-eat.com',
-		to: req.body.attendees,
-		subject: req.body.tripOwner + ' has invited you',
+		to: recipients,
+		subject: senderName + ' has invited you',
 		text: 'Use this link to accept or decline: ' + url,
 		html: `<p>Use this link to accept or decline: <a href="${url}">${url}</a></p>`
-	}, ()=> res.sendStatus(200))
-})
+	}, callback)
+}
