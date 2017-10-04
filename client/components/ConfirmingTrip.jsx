77629db845d8341e5c9b94f688ee896fede632bf
Sam <samuelglass@gmail.com>
2017-10-04 12:13:50 -0400

    before

--- client/components/ConfirmingTrip.jsx
+++ client/components/ConfirmingTrip.jsx
@@ -1,13 +1,19 @@
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import React, {Component} from 'react'
-// import {Col, Row, Button, Table} from 'react-bootstrap'
+import {Col, Row, Button, Table} from 'react-bootstrap'
 import {declineInvitation, setCoordinates} from './../store'
+
 
 
 const ConfirmTrip = (props) => {
 
+	let attendeeArray = props.currentTrip.attendees
+	let isTripOwner = true
+
 	console.log('from confirming', props)
+
+	let attending = true
 
 	let lat, long
 	navigator.geolocation.getCurrentPosition(function(position) {
@@ -20,9 +26,51 @@ const ConfirmTrip = (props) => {
 
 	return (
 		<div>
-			<button onClick={() => giveCoords([lat,long],props.currentTrip.id)}>I'm coming, here's my coordinates</button>
-			<button onClick={() => removeSelf(props.currentTrip.id,props.user.id)}>I cannot attend</button>
+
+			{!attending ?
+
+
+				<div>
+					<button onClick={() => giveCoords([lat,long],props.currentTrip.id,props.user.id)}>I'm coming, here's my coordinates</button>
+					<button onClick={() => removeSelf(props.currentTrip.id,props.user.id)}>I cannot attend</button>
+				</div>
+				:
+				<Row>
+					<Col xs={1}></Col>
+					<Col xs={10}>
+						<h3>Here's Who's Coming!</h3>
+						<Table responsive>
+							<thead>
+								<tr>
+									<th>#</th>
+									<th>Name</th>
+									<th>location</th>
+								</tr>
+							</thead>
+							<tbody>
+								{
+									attendeeArray.map((attendee, idx) =>{
+										return (
+											<tr key={idx}>
+												<td>{idx + 1}</td>
+												<td>{attendee.user.name}</td>{//TODO:Change to name
+												}
+												{attendee.origin ? (<td>{attendee.origin}</td>) : (<td>Awaiting Reply</td>)}
+											</tr>
+										)
+									})
+								}
+							</tbody>
+						</Table>
+						{
+							isTripOwner ? (<Button className='tripButton'>Procced to Voting</Button>) : ''
+						}
+					</Col>
+					<Col xs={1}></Col>
+				</Row>
+			}
 		</div>
+
 	)
 }
 
@@ -43,8 +91,8 @@ const mapDispatch = (dispatch) => {
 		removeSelf(tripId,userId) {
 			dispatch(declineInvitation(tripId, userId))
 		},
-		giveCoords(coords,tripId) {
-			dispatch(setCoordinates(coords, tripId))
+		giveCoords(coords,tripId, userId) {
+			dispatch(setCoordinates(coords, tripId, userId))
 		}
 	}
 }
--- server/api/trip.js
+++ server/api/trip.js
@@ -1,10 +1,10 @@
 const router = require('express').Router()
-const {Trip, Attendee} = require('../db/models')
+const {Trip, Attendee, User} = require('../db/models')
 module.exports = router
 
 router.get('/:id', (req, res, next) => {
 	Trip.findById(req.params.id, {
-		include: [ Attendee ] 
+		include: [ {model: Attendee, include: [User]} ]
 	})
 		.then(trip => res.json(trip))
 		.catch(next)
