926297a05a27b9e720420a9ac5e246658028ca9f
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-10-04 12:06:28 -0400

    Email api takes single recipient ID

--- server/api/email.js
+++ server/api/email.js
@@ -15,20 +15,23 @@ let transporter = nodeMailer.createTransport({
 
 router.post('/invite', (req, res) => { //just using post because it allows request to have a body
 	if(req.body.tripName.match(/^[a-z0-9]+$/i)){ //check that the name is alphanumeric
-		const url = 'http://localhost:8080/trips/' + req.body.tripName //or 'http://coordin-Eat.herokuapp.com/trips/ + req.body.tripName
-		User.findById(req.body.user.id)
+		const url = 'http://localhost:8080/trips/' + req.body.tripName //or 'http://coordin-Eat.herokuapp.com/trips/ + req.body.tripName		
+		User.findById(req.user.id)
 			.then(user => {
-				sendMail(url, user.name, req.body.attendees, () => res.sendStatus(200))
+				User.findById(req.body.invitee)
+					.then(invitee => {
+						sendMail(url, user.name, invitee.email, () => res.sendStatus(200))						
+					})		
 			})
 	} else {
 		res.sendStatus(403)
 	}
 })
 
-function sendMail(url, senderName, recipients, callback){
+function sendMail(url, senderName, recipient, callback){
 	transporter.sendMail({
 		from: 'noreply@coordin-eat.com',
-		to: recipients,
+		to: recipient,
 		subject: senderName + ' has invited you',
 		text: 'Use this link to accept or decline: ' + url,
 		html: `<p>Use this link to accept or decline: <a href="${url}">${url}</a></p>`
