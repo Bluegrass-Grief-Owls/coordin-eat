579a9b115014dae8857795dd681892f2ff514c76
Jackson Sui <kiro705@gmail.com>
2017-10-05 15:06:05 -0400

    tried changing webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,30 @@
 module.exports = {
-  entry: './client/index.js',
-  output: {
-    path: __dirname,
-    filename: './public/bundle.js'
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.svg$|\.ttf?|\.woff$|\.woff2|\.eof|\.eot/,
-        loader: 'file-loader'
-      }
-    ]
-  }
+	entry: './client/index.js',
+	output: {
+		path: __dirname,
+		filename: './public/bundle.js'
+	},
+	devtool: 'source-map',
+	module: {
+		noParse: /(mapbox-gl)\.js$/,
+		rules: [
+			{
+				test: /\.jsx?$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel-loader'
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader'
+				]
+			},
+			{
+				test: /\.svg$|\.ttf?|\.woff$|\.woff2|\.eof|\.eot/,
+				loader: 'file-loader'
+			}
+		]
+	}
 }
