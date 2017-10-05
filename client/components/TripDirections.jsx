9d49395734dc00be3963fbeb42907d6c8e81e018
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-10-05 15:37:22 -0400

    Fix directions link

--- client/components/TripDirections.jsx
+++ client/components/TripDirections.jsx
@@ -8,11 +8,43 @@ class TripDirections extends React.Component {
 	constructor(props) {
 		super(props)
 		this.meetingPlace = JSON.parse(props.meetingPlace)
+
+		console.log('abt to filter', props.userId, typeof props.userId, props.currentTrip.attendees)
+		const me = props.currentTrip
+			.attendees
+			.find((attendee) => {
+				console.log(attendee.userId)
+				return (attendee.userId === props.userId)
+			})
+
+		if (me) {
+			this.currentLat = me.origin[0]
+			this.currentLong = me.origin[1]
+		}
+	}
+
+	componentWillReceiveProps(newProps) {
+		this.meetingPlace = JSON.parse(newProps.meetingPlace)
+		console.log('abt to filter', newProps.userId, newProps.currentTrip.attendees)
+
+		const me = newProps.currentTrip
+			.attendees
+			.find((attendee) => {
+				console.log(attendee.userId)
+				return (attendee.userId === newProps.userId)
+			})
+
+		if (me) {
+			this.currentLat = me.origin[0]
+			this.currentLong = me.origin[1]
+			console.log('currentLat', this.currentLat)
+		}
+
 	}
 
 	componentDidMount() {
 		mapboxgl.accessToken = 'pk.eyJ1Ijoic2FtZ2xhc3MiLCJhIjoiY2o2ODNod2c3MGJqNDM0bDdpNm9xNWFxaSJ9.zt0UYvQhCl8Lx6zH9pZ7-w'
-		console.log('coords:', this.meetingPlace.coordinates)		
+		console.log('coords:', this.meetingPlace.coordinates)
 		this.map = new mapboxgl.Map({
 			container: 'putMapHere',
 			style: 'mapbox://styles/mapbox/streets-v9',
@@ -26,9 +58,8 @@ class TripDirections extends React.Component {
 	}
 
 	render() {
-		const origin = { latitude: 40.7094191, longitude: -74.0084873 }
 		const googleMapsUrl =
-			`https://www.google.com/maps/dir/${origin.latitude},+${origin.longitude}/${this.meetingPlace.coordinates.latitude},${this.meetingPlace.coordinates.longitude}`
+			`https://www.google.com/maps/dir/${this.currentLat},+${this.currentLong}/${this.meetingPlace.coordinates.latitude},${this.meetingPlace.coordinates.longitude}`
 
 		return (
 			<Row>
@@ -51,7 +82,9 @@ class TripDirections extends React.Component {
  */
 const mapState = state => {
 	return {
-		meetingPlace: state.currentTrip.yelpString
+		meetingPlace: state.currentTrip.yelpString,
+		currentTrip: state.currentTrip,
+		userId: state.user.id
 	}
 }
 
