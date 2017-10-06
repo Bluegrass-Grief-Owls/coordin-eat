2b9d70bf18525d1fbd4bee4c6ed1d8325f2bb228
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-10-06 12:33:43 -0400

    email gatekeeper

--- server/api/gatekeepers.js
+++ server/api/gatekeepers.js
@@ -6,20 +6,26 @@ function isLoggedIn(req, res, next) {
 	} else {
 		res.status(403).send(
 			`<h3>403 forbidden</h3>
-			<p>Hi, Dan!<p>`
+			<p>Hi, Dan!</p>`
 		)
 	}
 }
 
 function isTripOwner(req, res, next) {
 	const tripId = req.body.tripId || req.params.tripId
+	
 	Trip.findById(tripId)
 		.then((trip) => {
-			if (trip.ownerId === req.user.id) {
+			if (trip && (trip.ownerId === req.user.id)) {
 				return next()
+			} else {
+				res.status(403).send(
+					`<h3>403 forbidden</h3>
+					<p>(I'm spying on you!)</p>`
+				)
 			}
 		})
-		.catch()
+		.catch(next)
 }
 
 function isAttendee(req, res, next) {
@@ -28,15 +34,16 @@ function isAttendee(req, res, next) {
 		include: [{ model: Attendee, include: [User] }]
 	})
 		.then((trip) => {
-			if (trip.attendees.any(attendee => attendee.userId === req.user.id)) {
+			if (trip && (trip.attendees.any(attendee => attendee.userId === req.user.id))) {
 				return next()
 			} else {
 				res.status(403).send(
 					`<h3>403 forbidden</h3>
-					<p>Hi, Dan!<p>`
+					<p>Hey! </p>`
 				)
 			}
 		})
+		.catch(next)
 }
 
 module.exports = { isLoggedIn, isTripOwner, isAttendee }
