31624dc7f1940ecf8dff0bd8504d1c83787b70a0
David Berger <dgaberger@gmail.com>
2017-10-06 11:14:10 -0400

    pretty happy right now

--- server/midpointAlgorithm/mockGetTravelTime.js
+++ server/midpointAlgorithm/mockGetTravelTime.js
@@ -2,6 +2,7 @@ const Chance = require('chance')
 const chance = new Chance()
 var math = require('mathjs')
 
+// =============== VECTOR STUFF =========================
 const addVect = (...arr) => {
 	const result = [0,0]
 	arr.map((el) => {
@@ -9,10 +10,6 @@ const addVect = (...arr) => {
 		result[1] += el[1]
 	})
 	return result
-}
-
-const subVect = (vec1, vec2) => {
-	return [vec1[0] - vec2[0], vec1[1] - vec2[1]]
 }
 
 const scaleVect = (vec1, scale) => {
@@ -24,26 +21,12 @@ const centerCoord = (...arr) => {
 	return scaleVect(result, 1 / arr.length)
 }
 
-// given a center (mean) coordinate, gives a random coordinate based on the normal distribution
-// has arguments for std and bound
-// const normalCoord = (center, stDev, deviationBound) => {
-// 	let vertDisplacement = chance.normal({dev: stDev})
-// 	vertDisplacement = Math.abs(vertDisplacement) < deviationBound ? vertDisplacement : deviationBound
-// 	let horizDisplacement = chance.normal({dev: stDev})
-// 	horizDisplacement = Math.abs(horizDisplacement) < deviationBound ? horizDisplacement : deviationBound
-// 	return [center[0] + vertDisplacement, center[1] + horizDisplacement]
-// }
+const maxDistanceFromCentroid = (centroid, origins) => {
+	const dists = origins.map(coord => math.distance(coord, centroid))
+	return math.max(dists)
+}
 
-// ======== For testing =========
-// const optimalPair = [-.1, .5]
-// const k = -1
-// const b = 2
-// const mocktravelDur = (xyPair) => {
-// 	return k * (Math.pow(xyPair[0]-optimalPair[0], 2) + Math.pow(xyPair[1]-optimalPair[1], 2)) + b
-// }
-// ==============================
-
-
+// ================== POSSIBLE MEETUPS =====================
 // creates circles of new meetup candidates from a center coordinate
 const createCandidates = (n, center, bound) => {
 	const radiiRatio = [.005, .1,.2,.5, .9]
@@ -68,36 +51,7 @@ const createCandidates = (n, center, bound) => {
 	return result
 }
 
-const coordAnneal = (center, startRadius) => {
-	let currentCenter = center || [0,0]
-	let radius = startRadius || 1
-	let resolved = false // for closing the calculation early
-	let maxIterations = 3
-	let score
-	let cands
-	let indexOfMax
-	while (!resolved && maxIterations > 0) {
-		cands = createCandidates(10, currentCenter, radius)
-		score = cands.map(cand => cand)
-	
-		indexOfMax = score.indexOf(Math.max.apply(Math, score))
-		currentCenter = cands[indexOfMax]
-
-		radius *= .6
-
-		maxIterations--
-		// console.log(cands, score)
-	}
-	return cands[indexOfMax]
-}
-
-// console.log(createCandidates(4, [0,0], 20))
-// console.log(coordAnneal([0,0], .7))
-
-
-
-
-// =======================================
+// ==================== API REQUESTS & STUFF ===================
 
 const client = require('@google/maps').createClient({
 	key: 'AIzaSyB6N413BwZWE2KkNTdb0wsxQZw6VlrUvdU',
@@ -112,14 +66,13 @@ const getTravelTime = (origin, dest, mode) => {
 	}
 	return client.distanceMatrix(query).asPromise()
 		.then(res => {
-			console.log('responseeeee', res.json.rows[0].elements[0])
+			// console.log('response: ', res.json.rows[0].elements[0])
 			return res.json.rows[0].elements[0].duration.value / 60.0
 		})
 		.catch(console.log)
 }
 
-// getTravelTime([40.739999, -73.983083], [40.768007, -74.204254], 'transit')
-// 	.then(console.log)
+// ====================== PROMISES/SCORING =========================
 
 const locationPromiseAll = (loc, origins) => {
 	const locationsPromises = origins.map(origin => {
@@ -128,7 +81,9 @@ const locationPromiseAll = (loc, origins) => {
 	return Promise.all(locationsPromises)
 }
 
-const importance = 1
+const importance = .7 	
+// used for scaling the importance of 
+// similar travel times vs. total time traveled per group
 const locationScore = (loc, origins) => {
 	return locationPromiseAll(loc, origins)
 		.then(times => {
@@ -148,73 +103,21 @@ const candScorePromiseAll = (candArr, origins) => {
 }
 
 
-
-// candScorePromiseAll (
-// 	[
-// 		[ 40.80964019275301, -73.9270895692334 ],
-// 		[ 40.85964019275301, -73.9370895692334 ]
-// 	],
-// 	[
-// 		[ 40.76588646255421, -73.9825951401366 ],
-// 		[ 40.7971972524459, -73.93779144673098 ],
-// 		[ 40.66975627373278, -73.93889311219257 ],
-// 		[ 40.76525691298981, -73.9834959844067 ]
-// 	]
-// ).then(console.log)
-//	.catch(err => console.log(err))
-
-// =================================
-const investigate = (center, score, bound, numCands, origins) => {
-	const cands = createCandidates(numCands, center, bound)
-	// console.log('cands', cands)
-	// console.log('...origins', origins)
-	return candScorePromiseAll(cands, origins)
-		.then(scores => {
-			console.log('scores', scores)
-			const indexOfMax = scores.indexOf(Math.max.apply(Math, score))	
-			console.log('here yet')
-			const winner = score > scores[indexOfMax] ? [center, score] : [cands[indexOfMax], scores[indexOfMax]]
-			return winner	
-		})
-		.catch(err => console.log(err))
-}
-
-// investigate (
-// 	[ 40.80964019275301, -73.9270895692334 ],
-// 	123, .05, 10,
-// 	[
-// 		[ 40.76588646255421, -73.9825951401366 ],
-// 		[ 40.7971972524459, -73.93779144673098 ],
-// 		[ 40.66975627373278, -73.93889311219257 ],
-// 		[ 40.76525691298981, -73.9834959844067 ] 
-// 	]
-// )
-// 	.then(success => {
-// 		console.log('made it!!!!!')
-// 		console.log(success)
-// 	})
-// 	.catch(err => console.log(err))
-
-
-//============================================================
+//========================== THE ANNEALING =============================
 let origins = [
 	[40.709202, -73.957048],
-	[40.674333, -73.970488]
+	[40.674333, -73.970488],
+	[40.753504, -73.993079] 
 ]
 
-const maxDistanceFromCentroid = (centroid, origins) => {
-	const dists = origins.map(coord => math.distance(coord, centroid))
-	return math.max(dists)
-}
-
 const invest = (origins = [], n, center, lowScore = 9999) => {
-	const centroid = center || centerCoord(...origins).reverse()	
-	console.log(centroid.reverse())
+	const centroid = center || centerCoord(...origins)	
+	console.log(centroid)
 	const calcBound = maxDistanceFromCentroid(centroid, origins)
-	let cands = createCandidates(25, centroid.reverse(), calcBound * 2.5)
-	cands = cands.map(coord => coord.reverse())
+	let cands = createCandidates(25, centroid, calcBound * 2.5)
+	cands = cands.map(coord => coord)
 	console.log(cands)
-	// origins = origins.map(coord => coord.reverse())
+	// origins = origins.map(coord => coord)
 	console.log(origins)
 	return candScorePromiseAll(cands, origins)
 		.then(scores => {
