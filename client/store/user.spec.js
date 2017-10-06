d3440416297c8662cebad58c3e027b96dfae7dc9
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-10-06 16:37:40 -0400

    User spec uses tabs

--- client/store/user.spec.js
+++ client/store/user.spec.js
@@ -1,7 +1,7 @@
 /* global describe beforeEach afterEach it */
 
-import {expect} from 'chai'
-import {me, logout} from './user'
+import { expect } from 'chai'
+import { me, logout } from './user'
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
 import configureMockStore from 'redux-mock-store'
@@ -13,40 +13,40 @@ const middlewares = [thunkMiddleware]
 const mockStore = configureMockStore(middlewares)
 
 describe('thunk creators', () => {
-  let store
+	let store
 
-  const initialState = {user: {}}
+	const initialState = { user: {} }
 
-  beforeEach(() => {
-    store = mockStore(initialState)
-  })
+	beforeEach(() => {
+		store = mockStore(initialState)
+	})
 
-  afterEach(() => {
-    store.clearActions()
-  })
+	afterEach(() => {
+		store.clearActions()
+	})
 
-  describe('me', () => {
-    it('eventually dispatches the GET USER action', () => {
-      const fakeUser = {email: 'Cody'}
-      mockAxios.onGet('/auth/me').replyOnce(200, fakeUser)
-      return store.dispatch(me())
-        .then(() => {
-          const actions = store.getActions()
-          expect(actions[0].type).to.be.equal('GET_USER')
-          expect(actions[0].user).to.be.deep.equal(fakeUser)
-        })
-    })
-  })
+	describe('me', () => {
+		it('eventually dispatches the GET USER action', () => {
+			const fakeUser = { email: 'Cody' }
+			mockAxios.onGet('/auth/me').replyOnce(200, fakeUser)
+			return store.dispatch(me())
+				.then(() => {
+					const actions = store.getActions()
+					expect(actions[0].type).to.be.equal('GET_USER')
+					expect(actions[0].user).to.be.deep.equal(fakeUser)
+				})
+		})
+	})
 
-  describe('logout', () => {
-    it('logout: eventually dispatches the REMOVE_USER action', () => {
-      mockAxios.onPost('/auth/logout').replyOnce(204)
-      return store.dispatch(logout())
-        .then(() => {
-          const actions = store.getActions()
-          expect(actions[0].type).to.be.equal('REMOVE_USER')
-          expect(history.location.pathname).to.be.equal('/login')
-        })
-    })
-  })
+	describe('logout', () => {
+		it('logout: eventually dispatches the REMOVE_USER action', () => {
+			mockAxios.onPost('/auth/logout').replyOnce(204)
+			return store.dispatch(logout())
+				.then(() => {
+					const actions = store.getActions()
+					expect(actions[0].type).to.be.equal('REMOVE_USER')
+					expect(history.location.pathname).to.be.equal('/login')
+				})
+		})
+	})
 })
