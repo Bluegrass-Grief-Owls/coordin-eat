4bc156a2dda4bde7f115d331224e79924a4b870f
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-10-06 12:37:33 -0400

    Midpoint alg uses a gatekeeper

--- server/api/midpoint.js
+++ server/api/midpoint.js
@@ -1,15 +1,12 @@
 const router = require('express').Router()
 const midpointAlgorithm = require('../midpointAlgorithm')
+const { isLoggedIn } = require('./gatekeepers')
 module.exports = router
 
-router.post('/', (req, res, next) => { //just using post because it allows request to have a body
+router.post('/', isLoggedIn, (req, res, next) => { //just using post because it allows request to have a body
 	console.log('**', (req.body))
 	const point = midpointAlgorithm((req.body.places))
 	console.log("THIS IS THE POINT", Array.isArray(point))
 	console.log("0th index", point[0])
 	res.json(point)
-
-
-
-
 })
