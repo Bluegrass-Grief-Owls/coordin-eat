3f6d2e3664859456e3e4a5940c1463967d17f10c
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-10-06 16:55:14 -0400

    Remove some commented stuff

--- getTravelTime.js
+++ getTravelTime.js
@@ -12,14 +12,6 @@ const getTravelTime = (origin, dest, mode) => {
 	return client.distanceMatrix(query).asPromise()
 		.then(res => res.json.rows[0].elements[0].duration.value / 60.0)
 		.catch(console.error.bind(console))
-
-	// 	, (res, status) => {
-	// 	const tTime = res.rows.elements[0].duration.value / 60.0
-	// 	console.log('time is', tTime)
-	// })
 }
-
-// 	getTravelTime([40.739999, -73.983083], [40.768007, -74.204254], 'transit')
-// .then(console.log)
 
 export default getTravelTime
