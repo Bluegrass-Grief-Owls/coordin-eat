157153a420805de5b55cedb68900c25feca46f6c
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-10-09 17:57:48 -0400

    Get user uses req.user; fetchMyTrips changed to make use of this

--- client/components/UserHome.jsx
+++ client/components/UserHome.jsx
@@ -1,26 +1,19 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import {Col, Row, Button, Table, ListGroup, ListGroupItem} from 'react-bootstrap'
-import {fetchMyTrips, resetCurrentTrip, resetYelpList} from './../store'
-import {connect} from 'react-redux'
+import { Col, Row, Button, Table, ListGroup, ListGroupItem } from 'react-bootstrap'
+import { fetchMyTrips, resetCurrentTrip, resetYelpList } from './../store'
+import { connect } from 'react-redux'
 import history from './../history'
 
 /**
  * COMPONENT
  */
 class UserHome extends Component {
-
-	componentDidMount () {
-		this.props.loadInitialData(+this.props.user.id)
+	componentDidMount() {
+		this.props.loadInitialData()
 	}
 
-	componentDidUpdate(newProps) {
-		if(!newProps.myTrips[0]){
-			this.props.loadTrips(+this.props.user.id)
-		}
-	}
-
-	render () {
+	render() {
 		return (
 			<Row className=''>
 				<h3 className='noMargin welcomeUser'>Welcome, {this.props.user.name}!</h3>
@@ -29,13 +22,13 @@ class UserHome extends Component {
 					</Col>
 					<Col xs={10}>
 						<ListGroup className='myTripList'>
-							{ this.props.myTrips.length ?
+							{this.props.myTrips.length ?
 								this.props.myTrips.map(attendance => {
 									//This is actually a list of attendances attached with the trip they attended
 									let theTrip = attendance.trip
 									//TODO: if trip gets deleted need to specify that all associated attendences need to be deleted
-									return(
-										<ListGroupItem key={theTrip.id} className='noPaddingBottom myTripList' onClick={() => {history.push(`/trip/${theTrip.id}`)}}>
+									return (
+										<ListGroupItem key={theTrip.id} className='noPaddingBottom myTripList' onClick={() => { history.push(`/trip/${theTrip.id}`) }}>
 											<h3 className='noMargin fontMainColor'>{theTrip.name}</h3>
 											<h5 className='fontMainColor'>Date: {theTrip.readableDate}</h5>
 											<h5 className='fontMainColor'>Time: {theTrip.time}</h5>
@@ -67,17 +60,10 @@ const mapState = (state) => {
 
 const mapDispatch = (dispatch) => {
 	return {
-		loadInitialData (userId) {
+		loadInitialData() {
 			dispatch(resetCurrentTrip())
 			dispatch(resetYelpList())
-			if(userId){
-				dispatch(fetchMyTrips(userId))
-			}
-		},
-		loadTrips (userId) {
-			if(userId){
-				dispatch(fetchMyTrips(userId))
-			}
+			dispatch(fetchMyTrips())
 		}
 	}
 }
