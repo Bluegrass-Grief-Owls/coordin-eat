538844ae75be87261aacaee50b245b10df85663e
Jackson Sui <kiro705@gmail.com>
2017-10-10 18:02:42 -0400

    fixed adding friends to get user

--- server/auth/index.js
+++ server/auth/index.js
@@ -11,7 +11,8 @@ router.post('/login', (req, res, next) => {
 				res.status(401).send('Incorrect password')
 			} else {
 				//Returns user info, but not the password or salt
-				req.login(user, err => err ? next(err) : res.json({id: user.id, email: user.name, name: user.name, favoriteFood: user.favoriteFood}))
+				console.log(user)
+				req.login(user, err => err ? next(err) : res.json({id: user.id, email: user.name, name: user.name, favoriteFood: user.favoriteFood, friend: user.friend}))
 			}
 		})
 		.catch(next)
