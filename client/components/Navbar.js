4830a1fe530d878769b2fa22dfb71dfff1c0c9d5
Jackson Sui <kiro705@gmail.com>
2017-10-10 11:30:01 -0400

    working on navbar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -7,26 +7,26 @@ const navbarInstance = (props) => {
 	console.log(isLoggedIn)
 
 	return (
-		<Navbar collapseOnSelect className="noMargin">
+		<Navbar collapseOnSelect fixedTop={true} className="noMargin">
 			<Navbar.Header>
 				<Navbar.Brand className="noMargin">
-					<Link to="/home">Coordin-EAT</Link>
+					<Link to="/home" className='navTitle'>Coordin-EAT</Link>
 				</Navbar.Brand>
 				<Navbar.Toggle />
 			</Navbar.Header>
 			{
 				isLoggedIn
 					? <div>
-						<Navbar.Collapse>
+						<Navbar.Collapse className='noPadding'>
 							<Nav>
-								<NavItem className='noMargin noPadding' eventKey={1}>
-									<a className='noMargin noPadding' href='#' onClick={handleClick}><h4 className="displayInline">Logout</h4></a>
+								<NavItem eventKey={1}>
+									<a href='#' onClick={handleClick}><h4 className="fontMainColor displayInline">Logout</h4></a>
 								</NavItem>
-								<NavItem className='noMargin noPadding' eventKey={1}>
-									<Link className='noMargin noPadding' to='/build_trip'><h4 className="displayInline">Build a Trip</h4></Link>
+								<NavItem eventKey={1}>
+									<Link to='/build_trip'><h4 className="fontMainColor displayInline">Build a Trip</h4></Link>
 								</NavItem>
-								<NavItem className='noMargin noPadding' eventKey={1}>
-									<Link className='noMargin noPadding' to='/friends'><h4 className="displayInline">Friends</h4></Link>
+								<NavItem eventKey={1}>
+									<Link to='/friends'><h4 className="fontMainColor displayInline">Friends</h4></Link>
 								</NavItem>																
 							</Nav>
 						</Navbar.Collapse>
