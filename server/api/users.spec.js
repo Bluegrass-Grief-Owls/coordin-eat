932e4fe54eb2036a473fd1de8b0750ca9c2d1ea2
Jackson Sui <kiro705@gmail.com>
2017-10-18 17:15:04 -0400

    all tests pass!

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -6,30 +6,45 @@ const db = require('../db')
 const app = require('../index')
 const User = db.model('user')
 
+const codysEmail = 'cody@puppybook.com'
+const codysName = 'Cody Dog'
+const codysPassword = 'bones'
+
+function promisedCookie() {
+	return new Promise((resolve, reject) => {
+		request(app).post('/auth/login').send({name: codysName, email: codysEmail, password: codysPassword})
+			.end(function(error, response) {
+				if (error) reject(error)
+				var loginCookie = response.headers['set-cookie']
+				resolve(loginCookie)
+			})
+	})
+}
+
 describe('User routes', () => {
 	beforeEach(() => {
 		return db.sync({force: true})
 	})
 
 	describe('/api/users/', () => {
-		const codysEmail = 'cody@puppybook.com'
-		const codysName = 'Cody'
 
 		beforeEach(() => {
-			return User.create({
-				name: codysName,
-				email: codysEmail
-			})
+			return User.create({name: codysName, email: codysEmail, password: codysPassword})
 		})
 
 		it('GET /api/users', () => {
-			return request(app)
-				.get('/api/users')
-				.expect(200)
-				.then(res => {
-					expect(res.body).to.be.an('array')
-					expect(res.body[0].email).to.be.equal(codysEmail)
-				})
+			return promisedCookie().then(cookie => {
+				console.log('cookie is called', cookie)
+				const req = request(app)
+					.get('/api/users')
+					.set('cookie', cookie)
+					.expect(200)
+					.then(res => {
+						expect(res.body).to.be.an('array')
+						expect(res.body[0].email).to.be.equal(codysEmail)
+					})
+				return req
+			})
 		})
 	}) // end describe('/api/users')
 }) // end describe('User routes')
